29cff0ff8314c60c1c577a9a9f0ed26aa99efdba
Guilherme Leite <guilhermegals@gmail.com>
2021-10-03 19:10:08 -0300

    Criação da tela de adição de registros

--- script.js
+++ script.js
@@ -7,12 +7,61 @@ onload = () => {
     }
 
     document.querySelector('#addRecordButton').onclick = () => {
+        clearFields();
         show('screenAddRecord');
+    };
+
+    document.querySelector('#addRecordBackButton').onclick = () => {
+        show('screenHome');
+    };
+
+    document.querySelector('#addRecordSaveButton').onclick = () => {
+        addRecord()
     };
 };
 
 const show = (comp) => {
-    let listaDeTelas = document.querySelectorAll('body > .screen');
-    listaDeTelas.forEach((c) => c.classList.add('hidden'));
+    document.querySelectorAll('body > .screen').forEach((c) => c.classList.add('hidden'));
     document.querySelector('#' + comp).classList.remove('hidden');
-  };
+};
+
+const addRecord = () => {
+    let title = window.document.getElementById('inputTitle').value;
+    let date = window.document.getElementById('inputDate').value;
+    let type = window.document.getElementById('inputType').value;
+    let quantity = window.document.getElementById('inputValue').value;
+
+    if (title && date && type && quantity) {
+        records.push({
+            id: Math.random().toString().replace('0.', ''),
+            title: title,
+            date: date,
+            type: type,
+            quantity: quantity
+        });
+
+        sortRecords();
+        saveRecords();
+        clearFields();
+        show('screenHome');
+    } else {
+        alert('Verifique se preencheu os campos corretamente')
+    }
+};
+
+const clearFields = () => {
+    window.document.getElementById('inputTitle').value = '';
+    window.document.getElementById('inputDate').value = '';
+    window.document.getElementById('inputType').value = '';
+    window.document.getElementById('inputValue').value = '';
+};
+
+const sortRecords = () => {
+    records.sort(function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+    });
+}
+
+const saveRecords = () => {
+    localStorage.setItem('records', JSON.stringify(records)); 
+}
