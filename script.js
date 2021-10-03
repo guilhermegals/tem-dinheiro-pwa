cff6a078086001a93cf80f9671b809cf1b51648c
Guilherme Leite <guilhermegals@gmail.com>
2021-10-03 20:45:45 -0300

    Criação da edição de registros.

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ onload = () => {
     document.querySelector('#addRecordSaveButton').onclick = () => {
         addRecord()
     };
+
+    document.querySelector('#updateRecordBackButton').onclick = () => {
+        show('screenHome');
+    };
+
+    document.querySelector('#updateRecordSaveButton').onclick = () => {
+        updateRecord()
+    };
+
+    document.querySelector('#updateRecordDeleteButton').onclick = () => {
+        deleteRecord();
+    };
 };
 
 const show = (comp) => {
@@ -51,11 +63,57 @@ const addRecord = () => {
     }
 };
 
+const updateRecord = () => {
+    let title = window.document.getElementById('inputUpdateTitle').value;
+    let date = window.document.getElementById('inputUpdateDate').value;
+    let type = window.document.getElementById('inputUpdateType').value;
+    let quantity = window.document.getElementById('inputUpdateValue').value;
+
+    if (title && date && type && quantity) {
+        let id = window.document.getElementById('recordId').innerText;
+        records = records.filter((record) => record.id != id);
+
+        records.push({
+            id: id,
+            title: title,
+            date: date,
+            type: type,
+            quantity: quantity
+        });
+
+        sortRecords();
+        saveRecords();
+        clearFieldsUpdate();
+        updateBalance();
+        show('screenHome');
+    } else {
+        alert('Verifique se preencheu os campos corretamente')
+    }
+};
+
+const deleteRecord = () => {
+    let id = window.document.getElementById('recordId').innerText;
+    records = records.filter((record) => record.id != id);
+
+    sortRecords();
+    saveRecords();
+    updateBalance();
+    clearFieldsUpdate();
+    show('screenHome');
+};
+
 const clearFields = () => {
     window.document.getElementById('inputTitle').value = '';
     window.document.getElementById('inputDate').value = '';
     window.document.getElementById('inputType').value = '';
     window.document.getElementById('inputValue').value = '';
+};
+
+const clearFieldsUpdate = () => {
+    window.document.getElementById('inputUpdateTitle').value = '';
+    window.document.getElementById('inputUpdateDate').value = '';
+    window.document.getElementById('inputUpdateType').value = '';
+    window.document.getElementById('inputUpdateValue').value = '';
 };
 
 const sortRecords = () => {
@@ -85,9 +143,21 @@ const updateBalance = () => {
         totalIncomes += +record.quantity;
     });
 
+    let balance = totalIncomes - totalExpenses;
     window.document.getElementById('totalExpenses').innerText = 'R$ ' + totalExpenses.toFixed(2);
     window.document.getElementById('totalIncomes').innerText = 'R$ ' + totalIncomes.toFixed(2);
-    window.document.getElementById('balanceValue').innerText = 'R$ ' + (totalIncomes - totalExpenses).toFixed(2);
+    window.document.getElementById('balanceValue').innerText = 'R$ ' + (balance).toFixed(2);
+
+    if(balance > 0){
+        window.document.getElementById('balanceValue').classList.remove('expenses');
+        window.document.getElementById('balanceValue').classList.add('incomes');
+    } else if (balance < 0) { 
+        window.document.getElementById('balanceValue').classList.remove('incomes');
+        window.document.getElementById('balanceValue').classList.add('expenses');
+    } else {
+        window.document.getElementById('balanceValue').classList.remove('expenses');
+        window.document.getElementById('balanceValue').classList.remove('incomes');
+    }
 
     if(records.length > 0){
         window.document.getElementById('noRecordsContent').classList.add('hidden');
@@ -118,6 +188,15 @@ const showRecords = () => {
         }
 
         template.setAttribute('id', record.id);
+
+        template.onclick = () => {
+            window.document.getElementById('recordId').innerText = record.id;
+            window.document.getElementById('inputUpdateTitle').value = record.title;
+            window.document.getElementById('inputUpdateDate').value = record.date;
+            window.document.getElementById('inputUpdateType').value = record.type;
+            window.document.getElementById('inputUpdateValue').value = record.quantity;
+            show('screenUpdateRecord');
+        };
 
         list.appendChild(template);
     });
