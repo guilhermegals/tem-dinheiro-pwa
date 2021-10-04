85cd94b78a62b3d6464fb80bb30584cedbf2d749
Guilherme Leite <guilhermegals@gmail.com>
2021-10-03 21:04:23 -0300

    Alteração do worker

--- worker.js
+++ worker.js
@@ -1,65 +1,64 @@
-// Define o nome do cache atual, considerando a sua versão.
-//var cacheName = 'tem-dinheiro';
+var cacheName = 'tem-dinheiro';
 
 self.addEventListener('install', function (event) {
     console.log("Worker: Installed")
 
-    // caches.open(cacheName).then((cache) => {
-    //     cache.addAll([
-    //         '/',
-    //         '/index.html',
-    //         '/manifest.webmanifest',
-    //         '/style.css',
-    //         '/script.js',
-    //         '/res/img/icons/favicon.ico',
-    //         '/res/img/icons/android-icon-36x36.png',
-    //         '/res/img/icons/android-icon-48x48.png',
-    //         '/res/img/icons/android-icon-72x72.png',
-    //         '/res/img/icons/android-icon-96x96.png',
-    //         '/res/img/icons/android-icon-144x144.png',
-    //         '/res/img/icons/android-icon-192x192.png',
-    //         '/res/img/icons/apple-icon-57x57.png',
-    //         '/res/img/icons/apple-icon-60x60.png',
-    //         '/res/img/icons/apple-icon-72x72.png',
-    //         '/res/img/icons/apple-icon-120x120.png',
-    //         '/res/img/icons/apple-icon-144x144.png',
-    //         '/res/img/icons/apple-icon-152x152.png',
-    //         '/res/img/icons/apple-icon-180x180.png'
-    //     ]);
-    // });
+    caches.open(cacheName).then((cache) => {
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/manifest.webmanifest',
+            '/style.css',
+            '/script.js',
+            '/res/img/icons/favicon.ico',
+            '/res/img/icons/android-icon-36x36.png',
+            '/res/img/icons/android-icon-48x48.png',
+            '/res/img/icons/android-icon-72x72.png',
+            '/res/img/icons/android-icon-96x96.png',
+            '/res/img/icons/android-icon-144x144.png',
+            '/res/img/icons/android-icon-192x192.png',
+            '/res/img/icons/apple-icon-57x57.png',
+            '/res/img/icons/apple-icon-60x60.png',
+            '/res/img/icons/apple-icon-72x72.png',
+            '/res/img/icons/apple-icon-120x120.png',
+            '/res/img/icons/apple-icon-144x144.png',
+            '/res/img/icons/apple-icon-152x152.png',
+            '/res/img/icons/apple-icon-180x180.png'
+        ]);
+    });
 });
 
 self.addEventListener('activate', (e) => {
     console.log("Worker: Activated")
 
-    // e.waitUntil(
-    //     caches.keys().then((keyList) => {
-    //         return Promise.all(
-    //             keyList.map((key) => {
-    //                 if (key !== cacheName) {
-    //                     return caches.delete(key);
-    //                 }
-    //             })
-    //         );
-    //     })
-    // );
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 });
 
 
 self.addEventListener('fetch', function (event) { 
     console.log("Worker: Fetched")
 
-    // let response = caches.open(cacheName).then((cache) => { 
-    //   return cache.match(event.request).then((resource) => { 
-    //     if (resource) 
-    //         return resource; 
+    let response = caches.open(cacheName).then((cache) => { 
+      return cache.match(event.request).then((resource) => { 
+        if (resource) 
+            return resource; 
         
-    //     return fetch(event.request).then((resource) => { 
-    //       cache.put(event.request, resource.clone()); 
-    //       return resource; 
-    //     }); 
-    //   }); 
-    // }); 
+        return fetch(event.request).then((resource) => { 
+          cache.put(event.request, resource.clone()); 
+          return resource; 
+        }); 
+      }); 
+    }); 
     
-    // event.respondWith(response); 
+    event.respondWith(response); 
   });
